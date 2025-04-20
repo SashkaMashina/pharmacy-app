fef58c1be3c28873c1397ec2ad79d7fbdb2de911
MargoRita99 <makarova.mar93@gmail.com>
2025-04-20 22:43:04 +0300

    modified base.py

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -2,3 +2,6 @@ from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
+
+# Импорты всех моделей, чтобы Alembic их увидел
+from app.models import models  # пусть models.py содержит все классы
