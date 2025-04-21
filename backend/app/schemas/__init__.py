705e2cae90160604be123f9ec8d0a2385780cdc9
MargoRita99 <makarova.mar93@gmail.com>
2025-04-21 20:09:22 +0300

    modified __init__.py

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,4 +1,4 @@
 from .category import CategoryCreate, CategoryUpdate, CategoryResponse
 from .product import ProductCreate, ProductUpdate, ProductResponse
-from .reservation import ReservationCreate, ReservationUpdate, ReservationResponse
+from .reservation import ReservationCreate, ReservationResponse
 from .admin import LoginRequest, TokenResponse
