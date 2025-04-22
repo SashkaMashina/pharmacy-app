e5a658a67e05dba9027c3fe7f05370abc4a56178
Мистер Фокс <mah362026@gmail.com>
2025-04-22 18:21:55 +0300

    Add component button + globa styles

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,24 +1,13 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import React from "react";
+import { Htag } from "@/components";
+import { Button } from "@/components";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Pharmacy-app</title>
-      </Head>
+    <Htag tag="h1">Текст</Htag>
+    <Button appearance="primary" arrow="right">Кнопка</Button>
+    <Button appearance="ghost">Вторая кнопка</Button>
     </>
   );
 }
