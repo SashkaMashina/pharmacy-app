9e80c273cce1980a98b4e1ca9655de5969055de9
MargoRita99 <makarova.mar93@gmail.com>
2025-04-23 20:26:49 +0300

    added info for category_router

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,12 +8,15 @@ from app.middlewares.jwt_middleware import JWTMiddleware
 from app.routers.auth_router import router as auth_router
 from app.routers.admin_router import router as admin_router
 from app.routers.product_router import router as product_router
+from app.routers.category_router import router as category_router
 
 
 app = FastAPI(title="Pharmacy API")
 app.include_router(auth_router)
 app.include_router(admin_router)
 app.include_router(product_router)
+app.include_router(category_router)
+
 app.add_middleware(JWTMiddleware)
 
 
