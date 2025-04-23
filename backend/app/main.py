f738e35121a602816b073145e929ff7e1e2ea476
MargoRita99 <makarova.mar93@gmail.com>
2025-04-23 21:50:13 +0300

    added route for reservations

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,6 +9,7 @@ from app.routers.auth_router import router as auth_router
 from app.routers.admin_router import router as admin_router
 from app.routers.product_router import router as product_router
 from app.routers.category_router import router as category_router
+from app.routers.reservation_router import router as reservation_router
 
 
 app = FastAPI(title="Pharmacy API")
@@ -16,6 +17,7 @@ app.include_router(auth_router)
 app.include_router(admin_router)
 app.include_router(product_router)
 app.include_router(category_router)
+app.include_router(reservation_router)
 
 app.add_middleware(JWTMiddleware)
 
