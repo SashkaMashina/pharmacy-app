6aaa080833d24af30752a323cbb3d808a2f01b88
MargoRita99 <makarova.mar93@gmail.com>
2025-04-24 23:50:50 +0300

    added endpoints for reservations items

--- backend/app/routers/reservation_item_router.py
+++ backend/app/routers/reservation_item_router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 from app.core.database import get_db
 from app.models import models
 from app.schemas.reservation_item import ReservationItemResponse
@@ -8,18 +9,25 @@ router = APIRouter(prefix="/reservation_items", tags=["Reservation Items"])
 
 # Получить все зарезервированные товары
 @router.get("/", response_model=list[ReservationItemResponse])
-def get_all_items(db: Session = Depends(get_db)):
-    return db.query(models.ReservationItem).all()
+async def get_all_items(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(models.ReservationItem))
+    items = result.scalars().all()
+    return items
 
 # Получить товар по ID
 @router.get("/{id}", response_model=ReservationItemResponse)
-def get_item_by_id(id: int, db: Session = Depends(get_db)):
-    item = db.query(models.ReservationItem).filter(models.ReservationItem.id == id).first()
+async def get_item_by_id(id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(models.ReservationItem).where(models.ReservationItem.id == id))
+    item = result.scalar_one_or_none()
     if not item:
         raise HTTPException(status_code=404, detail="Reservation item not found")
     return item
 
 # Получить все товары по ID бронирования
 @router.get("/by_reservation/{reservation_id}", response_model=list[ReservationItemResponse])
-def get_items_by_reservation(reservation_id: int, db: Session = Depends(get_db)):
-    return db.query(models.ReservationItem).filter(models.ReservationItem.reservation_id == reservation_id).all()
+async def get_items_by_reservation(reservation_id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(
+        select(models.ReservationItem).where(models.ReservationItem.reservation_id == reservation_id)
+    )
+    items = result.scalars().all()
+    return items
