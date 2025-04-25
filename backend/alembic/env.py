a048ef464e501021bbc391fb7d3de1480d1d0fdf
nyutdf2 <annslob@gmail.com>
2025-04-25 16:41:33 +0300

    modified

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -32,14 +32,12 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
-
 async def run_migrations_online() -> None:
     """Запуск миграций в online-режиме (с подключением к БД)."""
     connectable = create_async_engine(DATABASE_URL, poolclass=pool.NullPool)
 
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
-
 
 def do_run_migrations(connection):
     """Функция, выполняющая миграции в синхронном режиме."""
@@ -48,7 +46,6 @@ def do_run_migrations(connection):
     )
     with context.begin_transaction():
         context.run_migrations()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
